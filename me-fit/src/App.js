b44dfb3b50c602ad6b4066ecd3d673e5b990ddee
Jim <buissinkjim@gmail.com>
2023-03-09 16:03:15 +0100

    ADD: basic navbar with routing

--- me-fit/src/App.js
+++ me-fit/src/App.js
@@ -1,10 +1,29 @@
+
 import './App.css';
+import NavBar from './components/Navbar/Navbar';
 import Dashboard from "./pages/Dashboard"
+import Profile from "./pages/Profile"
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
 
 function App() {
   return (
-    <Dashboard />
+    <div className='App'>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='*' element={
+            <>
+              <h1>There's nothing here 👻</h1>
+              <NavLink to="/">Return Home</NavLink>
+            </>
+          } />
+        </Routes>
+      </BrowserRouter>
 
+    </div>
   );
 }
 
