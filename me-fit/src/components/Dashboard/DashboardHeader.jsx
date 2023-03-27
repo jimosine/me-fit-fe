deff3c423cb7ecb07900943521a823ea16620987
livings08 <marcob93@hotmail.com>
2023-03-27 15:56:20 +0200

    ADD Styling to project
    Add styling project

--- me-fit/src/components/Dashboard/DashboardHeader.jsx
+++ me-fit/src/components/Dashboard/DashboardHeader.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from 'react';
 // import './styling/DashboardHeader.css'
 
 const DashboardHeader = () => {
+  return (
+    <div className="dash-header">
+      <h1> Dashboard</h1>
+      <p>afdsadfafdsadsfasdfasfdfsafdsa</p>
+    </div>
+  );
+};
 
-
-    return (
-        <div className='dash-header'>
-            <h1> Dashboard</h1>
-        </div>
-    )
-}
-
-export default DashboardHeader
+export default DashboardHeader;
--- me-fit/src/components/Dashboard/GoalsList.jsx
+++ me-fit/src/components/Dashboard/GoalsList.jsx
@@ -1,70 +1,63 @@
-import GoalItem from "./GoalItem"
+import GoalItem from './GoalItem';
 import Accordion from 'react-bootstrap/Accordion';
-import { useEffect, useState } from "react";
-import { storageRead, storageSave } from "../../utils/storage";
+import { useEffect, useState } from 'react';
+import { storageRead, storageSave } from '../../utils/storage';
 
 const GoalsList = ({ goals, removeGoals, setGoals, loading, setLoading }) => {
+  useEffect(() => {
+    if (storageRead('goals') === null) {
+      setLoading(true);
+      const fetchData = async () => {
+        Promise.all([
+          // fetch('https://jb-lost-in-translation-api-production.up.railway.app/trainers'
+          // await fetch('https://cors-anywhere.herokuapp.com/https://me-fit-nl.azurewebsites.net/goal'),
+          await fetch('https://me-fit-nl.azurewebsites.net/goal'),
+        ])
+          .then(([response]) => Promise.all([response.json()]))
+          .then(async ([data]) => {
+            // setPrograms(dataPrograms);
+            storageSave('goals', data);
+            setGoals(data);
+            // addGoals(storageRead('goals'))
+            console.log('is nog leeg');
+            await setLoading(false);
+          });
+      };
 
+      fetchData();
+    } else {
+      setGoals(storageRead('goals'));
+    }
+  }, []);
 
-    useEffect(() => {
-        if (storageRead('goals') === null) {
-            setLoading(true);
-            const fetchData = async () => {
-                Promise.all([
-                    // fetch('https://jb-lost-in-translation-api-production.up.railway.app/trainers'
-                    // await fetch('https://cors-anywhere.herokuapp.com/https://me-fit-nl.azurewebsites.net/goal'),
-                    await fetch('https://me-fit-nl.azurewebsites.net/goal'),
-                ])
-                    .then(([response]) =>
-                        Promise.all([response.json()])
-                    )
-                    .then(async ([data]) => {
-                        // setPrograms(dataPrograms);
-                        storageSave('goals', data)
-                        setGoals(data)
-                        // addGoals(storageRead('goals'))
-                        console.log("is nog leeg");
-                        await setLoading(false);
-                    });
-            }
+  //Map over the goals array and create GoalItem components for each entry (goal)
+  // const goalsItems = goals.filter(goal => goal.profile === 3).map((goal, index) => <GoalItem key={goal.Name + '-' + index} index={index} goal={goal} removeGoals={removeGoals} />)
+  const goalsItems = goals.map((goal, index) => (
+    <GoalItem
+      key={goal.Name + '-' + index}
+      index={index}
+      goal={goal}
+      removeGoals={removeGoals}
+    />
+  ));
 
-            fetchData()
-
-        }
-        else {
-            setGoals(storageRead('goals'))
-        }
-    }, []);
-
-
-    //Map over the goals array and create GoalItem components for each entry (goal)
-    const goalsItems = goals.filter(goal => goal.profile === 3).map((goal, index) => <GoalItem key={goal.Name + '-' + index} index={index} goal={goal} removeGoals={removeGoals} />)
-
-
-    return (
-
-        // Only render the goal list if there is a least one goal present
-        <div className="GoalList">
-
-            {/* MISSCHIEN WEL ZO DOEN */}
-            {/* {loading && <p> Loading goals.. </p>}
+  return (
+    // Only render the goal list if there is a least one goal present
+    <div className="GoalList">
+      {/* MISSCHIEN WEL ZO DOEN */}
+      {/* {loading && <p> Loading goals.. </p>}
             {goals.length === 0 && <p>Currently no goals.</p>} */}
-            {loading ? <p> Loading goals.. </p> : goals.length === 0 && <p>Currently no goals.</p>}
+      {loading ? (
+        <p> Loading goals.. </p>
+      ) : (
+        goals.length === 0 && <p>Currently no goals.</p>
+      )}
 
-            {goals.length !== 0 &&
+      {goals.length !== 0 && (
+        <Accordion className="goals-accordion">{goalsItems}</Accordion>
+      )}
+    </div>
+  );
+};
 
-                <Accordion className="goals-accordion">
-                    {goalsItems}
-
-
-                </Accordion>
-            }
-
-        </div>
-
-
-    )
-
-}
-
-export default GoalsList
+export default GoalsList;
