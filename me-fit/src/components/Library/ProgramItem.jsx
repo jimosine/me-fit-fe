ded4bd509362a78e953a5f874de949103cdf51b8
livings08 <marcob93@hotmail.com>
2023-03-28 13:33:30 +0200

    UPDATE styling the webapi
    UPDATE styling the webapi.
    ADD achievments to the project

--- me-fit/src/components/Library/ProgramItem.jsx
+++ me-fit/src/components/Library/ProgramItem.jsx
@@ -5,71 +5,77 @@ import React, { useState } from 'react';
 import { storageRead } from '../../utils/storage';
 
 const ProgramItem = ({ cardTitle, selectedButton, program }) => {
-    const [lgShow, setLgShow] = useState(false);
+  const [lgShow, setLgShow] = useState(false);
 
-    //TO ALSO GET THE WORKOUT NAMES
-    const workoutsIds = program.workoutsId
-    const workouts = storageRead('workouts')
-    const correctWorkouts = workouts.filter(item => workoutsIds.includes(item.id))
-    const workoutNames = correctWorkouts.map(item => item.name)
+  //TO ALSO GET THE WORKOUT NAMES
+  const workoutsIds = program.workoutsId;
+  const workouts = storageRead('workouts');
+  const correctWorkouts = workouts.filter((item) =>
+    workoutsIds.includes(item.id)
+  );
+  const workoutNames = correctWorkouts.map((item) => item.name);
 
+  return (
+    <>
+      <Card className="cardStyle" style={{ width: '18rem' }}>
+        <Card.Img
+          variant="top"
+          src={program.imglink}
+          className="ProgramItem-img"
+        />
+        <Card.Body>
+          <Card.Title>{cardTitle}</Card.Title>
+          <Card.Text style={{ height: '85px' }}>
+            {program.description_short}
+          </Card.Text>
+          <Button
+            className="buttonEdit"
+            onClick={() => setLgShow(true)}
+            variant="success"
+          >
+            More info
+          </Button>
 
-    return (
-        <>
-
-            <Card style={{ width: '18rem' }}>
-                <Card.Img
-                    variant="top"
+          {/* For Programs */}
+          {selectedButton === 'Programs' && (
+            <Modal
+              size="lg"
+              show={lgShow}
+              onHide={() => setLgShow(false)}
+              aria-labelledby="example-modal-sizes-title-lg"
+              centered
+            >
+              <Modal.Header closeButton>
+                <Modal.Title id="example-modal-sizes-title-lg">
+                  {program.name}
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <div style={{ display: 'flex' }}>
+                  <img
                     src={program.imglink}
-                    className="ProgramItem-img" />
-                <Card.Body>
-                    <Card.Title>{cardTitle}</Card.Title>
-                    <Card.Text style={{ height: '85px' }}>
-
-                        {program.description_short}
-                    </Card.Text>
-                    <Button onClick={() => setLgShow(true)} variant="success">More info</Button>
-
-                    {/* For Programs */}
-                    {selectedButton === 'Programs' &&
-                        <Modal
-                            size="lg"
-                            show={lgShow}
-                            onHide={() => setLgShow(false)}
-                            aria-labelledby="example-modal-sizes-title-lg"
-                            centered
-                        >
-                            <Modal.Header closeButton>
-                                <Modal.Title id="example-modal-sizes-title-lg">
-                                    {program.name}
-                                </Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>
-                                <div style={{ display: 'flex' }}>
-                                    <img
-                                        src={program.imglink}
-                                        alt="exercise"
-                                        style={{ marginRight: '20px', width: '50%' }}
-                                    />
-                                    <div style={{ width: '50%' }}>
-                                        <h4>Type:</h4> <p>{program.type}</p>
-                                        <h4>Workouts:</h4>
-                                        <ul>
-                                            {workoutNames.map((item, index) => (<li key={index}>{item}</li>))}
-
-                                        </ul>
-
-                                        <h4>Description:</h4> <p>{program.description_long}</p>
-                                    </div>
-                                </div>
-                            </Modal.Body>
-                        </Modal>}
-
-                </Card.Body>
-            </Card></>
-    );
-}
+                    alt="exercise"
+                    style={{ marginRight: '20px', width: '50%' }}
+                  />
+                  <div style={{ width: '50%' }}>
+                    <h4>Type:</h4> <p>{program.type}</p>
+                    <h4>Workouts:</h4>
+                    <ul>
+                      {workoutNames.map((item, index) => (
+                        <li key={index}>{item}</li>
+                      ))}
+                    </ul>
+                    <h4>Description:</h4> <p>{program.description_long}</p>
+                  </div>
+                </div>
+              </Modal.Body>
+            </Modal>
+          )}
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
 
 // Export the component
-export default ProgramItem
-
+export default ProgramItem;
--- me-fit/src/pages/Library.jsx
+++ me-fit/src/pages/Library.jsx
@@ -1,132 +1,126 @@
-import LibraryHeader from "../components/Library/LibraryHeader"
-import LibraryList from "../components/Library/LibraryList";
+import LibraryHeader from '../components/Library/LibraryHeader';
+import LibraryList from '../components/Library/LibraryList';
 import { Row, Col } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import ContributionForm from "../components/Library/ContributionForm";
-import { isContributorRole } from "../utils/user"
-import { Navigate } from "react-router-dom";
+import ContributionForm from '../components/Library/ContributionForm';
+import { isContributorRole } from '../utils/user';
+import { Navigate } from 'react-router-dom';
+import { FaPlusCircle } from 'react-icons/fa';
 
-
-import { useState } from "react";
-import { storageDelete, storageSave } from "../utils/storage";
-
+import { useState } from 'react';
+import { storageDelete, storageSave } from '../utils/storage';
 
 const Library = ({ updateContributions, contributions }) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
+  const [selectedButton, setSelectedButton] = useState('Exercises');
 
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  // Define a function to handle button clicks
+  const handleButtonClick = (button) => {
+    setSelectedButton(button);
+  };
 
-    const [selectedButton, setSelectedButton] = useState('Exercises');
+  const handleFormSubmit = (data) => {
+    console.log('DATA TO BE POSTED: ');
 
-    // Define a function to handle button clicks
-    const handleButtonClick = (button) => {
-        setSelectedButton(button);
-    };
+    //handle de .value shit:
+    //PROGRAMS:
+    if (data.Type.value === 'Program') {
+      data.Type = data.Type.value;
+      data.programType = data.programType.value;
 
+      //Workout:
+    } else if (data.Type.value === 'Workout') {
+      data.Type = data.Type.value;
+      data.workoutType = data.workoutType.value;
 
-    const handleFormSubmit = (data) => {
+      //Exercise
+    } else if (data.Type.value === 'Exercise') {
+      data.Type = data.Type.value;
+      data.musclegroup = data.musclegroup.value;
+      console.log('posting exercise');
+    }
 
-        console.log("DATA TO BE POSTED: ");
+    console.log(data);
+    updateContributions([data]);
 
-        //handle de .value shit:
-        //PROGRAMS:
-        if (data.Type.value === "Program") {
-            data.Type = data.Type.value
-            data.programType = data.programType.value
+    //IF EXERCISE
+    if (data.Type === 'Exercise') {
+      console.log('DOING PUT NOW');
 
-            //Workout:
-        } else if (data.Type.value === "Workout") {
-            data.Type = data.Type.value
-            data.workoutType = data.workoutType.value
+      //EERST EEN POST MET ALLES BEHALVE WORKOUTS/EXERCISES
+      fetch('https://me-fit-nl.azurewebsites.net/exercise', {
+        // fetch("https://cors-anywhere.herokuapp.com/https://me-fit-nl.azurewebsites.net/exercise", {
+        method: 'POST',
+        body: JSON.stringify({
+          name: data.name,
+          description: data.description,
+          musclegroup: data.musclegroup,
+          vidlink: data.vidlink,
+          imglink: data.imglink,
+          repetitions: data.repetitions,
+          profileId: 3,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      }).then((response) => console.log(response));
+    }
 
-            //Exercise
-        } else if (data.Type.value === "Exercise") {
-            data.Type = data.Type.value
-            data.musclegroup = data.musclegroup.value
-            console.log("posting exercise")
-        }
+    // storageSave('contributions', contributions)
 
-        console.log(data);
-        updateContributions([data])
+    //SLUIT DE FORM MODAL
+    handleClose();
+  };
 
+  return (
+    <div>
+      <Row>
+        {/* <Col>Search stuff</Col> */}
+        <Col>
+          <LibraryHeader
+            selectedButton={selectedButton}
+            handleButtonClick={handleButtonClick}
+          />
+        </Col>
+        {/* <Col></Col> */}
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col xs={8}>
+          <LibraryList selectedButton={selectedButton} />
+        </Col>
+        <Col>
+          {isContributorRole() && (
+            <button className="add-goal-button">
+              <FaPlusCircle size={50} onClick={handleShow} />
+            </button>
+          )}
 
-        //IF EXERCISE
-        if (data.Type === "Exercise") {
+          <Modal
+            show={show}
+            onHide={handleClose}
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+            <Modal.Header
+              closeButton
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Modal.Title className="ms-auto">Contribution Form</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <ContributionForm onSubmit={handleFormSubmit} />
+            </Modal.Body>
 
-            console.log("DOING PUT NOW");
-
-            //EERST EEN POST MET ALLES BEHALVE WORKOUTS/EXERCISES
-            fetch("https://me-fit-nl.azurewebsites.net/exercise", {
-                // fetch("https://cors-anywhere.herokuapp.com/https://me-fit-nl.azurewebsites.net/exercise", {
-                method: "POST",
-                body: JSON.stringify({
-                    "name": data.name,
-                    "description": data.description,
-                    "musclegroup": data.musclegroup,
-                    "vidlink": data.vidlink,
-                    "imglink": data.imglink,
-                    "repetitions": data.repetitions,
-                    "profileId": 3
-                }),
-                headers: {
-                    "Content-type": "application/json; charset=UTF-8"
-                }
-            })
-                .then((response) => console.log(response))
-        }
-
-
-        // storageSave('contributions', contributions)
-
-
-
-        //SLUIT DE FORM MODAL
-        handleClose()
-
-
-    };
-
-    return (
-        <div>
-            <Row >
-                {/* <Col>Search stuff</Col> */}
-                <Col ><LibraryHeader selectedButton={selectedButton} handleButtonClick={handleButtonClick} /></Col>
-                {/* <Col></Col> */}
-            </Row>
-            <Row>
-                <Col>Filter stuff</Col>
-                <Col xs={8}><LibraryList selectedButton={selectedButton} /></Col>
-                <Col>
-                    {isContributorRole() && <Button variant="primary" onClick={handleShow}>
-                        Add new contribution
-                    </Button>}
-
-
-                    <Modal
-                        show={show}
-                        onHide={handleClose}
-                        aria-labelledby="contained-modal-title-vcenter"
-                        centered>
-                        <Modal.Header
-                            closeButton
-                            style={{
-                                display: "flex",
-                                justifyContent: "center",
-                                alignItems: "center",
-                            }} >
-                            <Modal.Title className="ms-auto" >Contribution Form</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                            <ContributionForm onSubmit={handleFormSubmit} />
-
-                        </Modal.Body>
-
-
-
-                        {/* <Modal.Footer>
+            {/* <Modal.Footer>
                             <Button variant="secondary" onClick={handleClose}>
                                 Close
                             </Button>
@@ -134,10 +128,10 @@ const Library = ({ updateContributions, contributions }) => {
                                 Save Changes
                             </Button>
                         </Modal.Footer> */}
-                    </Modal></Col>
-            </Row>
-
-        </div >
-    )
-}
-export default Library
+          </Modal>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+export default Library;
