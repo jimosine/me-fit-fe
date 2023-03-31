20095c483e33c379e6022a9443b99f195f33e3b6
Jim <buissinkjim@gmail.com>
2023-03-31 13:27:12 +0200

    UPDATE: clean remaining code

--- me-fit/src/routes/KeycloakRoute.jsx
+++ me-fit/src/routes/KeycloakRoute.jsx
@@ -1,16 +1,12 @@
 import { Navigate } from "react-router-dom";
 import keycloak from "../keycloak"
 
-function KeycloakRoute({children,role,redirectTo="/login"}) {
+function KeycloakRoute({ children, role, redirectTo = "/login" }) {
     if (!keycloak.authenticated) {
         return <Navigate replace to={redirectTo} />;
-    }else {
+    } else {
         return <>{children}</>;
     }
-
-    // if (keycloak.hasRealmRole(role)){
-    //     return <>{children}</>;
-    // }
 
 }
 export default KeycloakRoute
--- me-fit/src/utils/user.js
+++ me-fit/src/utils/user.js
@@ -1,10 +1,6 @@
 import keycloak from "../keycloak";
 
-// export default function isUserRole(){
-//     return keycloak.hasRealmRole("USER")
-// }
+export const isUserRole = () => keycloak.hasRealmRole("USER");
 
-export const isUserRole= ()=> keycloak.hasRealmRole("USER");
-
-export const isContributorRole= ()=> keycloak.hasRealmRole("CONTRIBUTER");
+export const isContributorRole = () => keycloak.hasRealmRole("CONTRIBUTER");
 
